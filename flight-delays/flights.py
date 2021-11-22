ebf4b21d68bf63831240d66b3a4e42955ccbf337
Deborah Ong <deborah.oyt@gmail.com>
2021-11-22 09:43:19 -0800

    add queries

--- flight-delays/flights.py
+++ flight-delays/flights.py
@@ -19,4 +19,25 @@ schema = "`date` STRING, `delay` INT, `distance` INT, `origin` STRING, `destinat
 
 #longest flight distance
 spark.sql("""SELECT distance, origin, destination FROM us_delay_flights_tbl WHERE distance > 1000 ORDER BY distance DESC""").show(10)
+#or written in equivalent Data-Frame API query
+from pyspark.sql.functions import col, desc
+(df.select("distance", "origin", "destination")
+    .where(col("distance")>1000)
+    .orderBy(desc("distance"))).show(100)
+
+#all flights with at least 2-hour delay
+spark.sql("""SELECT date, delay, origin, destination FROM us_delay_flights_tbl WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD' ORDER by delay DESC""").show(10)
+
+#labelling the extent of the delay
+spark.sql("""SELECT delay, origin, destination,
+            CASE
+                WHEN delay > 360 THEN 'Very Long Delays'
+                WHEN delay > 120 AND delay < 360 THEN 'Long Delays'
+                WHEN delay > 60 AND delay < 120 THEN 'Short Delays'
+                WHEN delay > 0 and delay < 60  THEN  'Tolerable Delays'
+                WHEN delay = 0 THEN 'No Delays'
+                ELSE 'Early'
+            END AS Flight_Delays
+            FROM us_delay_flights_tbl
+            ORDER BY origin, delay DESC""").show(100)
 
